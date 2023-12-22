aa49f0aa72691a47d86b551c3879f236221c5400
Alexsaha1 <saha.personally@gmail.com>
2023-12-21 21:27:47 -0500

    3 loc pr, grt title, get url

--- src/test/java/com/loop/test/day9_config_driver/T2_java_faker.java
+++ src/test/java/com/loop/test/day9_config_driver/T2_java_faker.java
@@ -6,12 +6,15 @@ public class T2_java_faker {
     @Test
     public void java_faker(){
         Faker faker = new Faker();
-        System.out.println(faker.name().fullName());
-        System.out.println(faker.numerify("240-302-1410"));
-        System.out.println(faker.letterify("?????"));
-        System.out.println(faker.bothify("##??##?"));
-        System.out.println(faker.address().fullAddress());
-        System.out.println(faker.finance().creditCard());
-        System.out.println(faker.chuckNorris().fact());
+        System.out.println("faker.name().firstName() = " + faker.name().fullName());
+        System.out.println("faker.numerify(\"###-###-####\") = " + faker.numerify("202-###-####"));
+        System.out.println("faker.letterify(\"?????\") = " + faker.letterify("?????"));
+        System.out.println("faker.bothify(\"##??##?\") = " + faker.bothify("##??##?"));
+        System.out.println("faker.address().fullAddress() = " + faker.address().fullAddress());
+        System.out.println("faker.finance().creditCard() = " + faker.finance().creditCard());
+        System.out.println("faker.chuckNorris().fact() = " + faker.chuckNorris().fact().replace("Chuck Norris", "Feyruz"));
+
+
     }
+
 }
