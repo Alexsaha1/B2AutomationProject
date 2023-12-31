251c64d3dba5db77484acb0cf19d99cdc37a8acd
Alexsaha1 <saha.personally@gmail.com>
2023-12-31 14:43:42 -0500

    day9.4

--- src/test/java/com/loop/test/utillites/BrowserUtils.java
+++ src/test/java/com/loop/test/utillites/BrowserUtils.java
@@ -1,28 +1,36 @@
 package com.loop.test.utillites;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.Set;
+
 import static org.testng.Assert.*;
 
 public class BrowserUtils {
 
     /**
      * validate if driver switched to expected url and title
+     *
      * @param driver
      * @param expectedUrl
      * @param expectedTitle
      * @author alex
-     * @author nadir
      * implements assertion
      */
-    public static void switchWindowAndValidate(WebDriver driver, String expectedUrl, String expectedTitle){
+    public static void switchWindowAndValidate(WebDriver driver, String expectedUrl, String expectedTitle) {
         expectedTitle = expectedTitle.toLowerCase();
         expectedUrl = expectedUrl.toLowerCase();
-        Set <String> windowHandles = driver.getWindowHandles();
-        for (String each : windowHandles){
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String each : windowHandles) {
             driver.switchTo().window(each);
-            if(driver.getCurrentUrl().toLowerCase().contains(expectedUrl)){
+            if (driver.getCurrentUrl().toLowerCase().contains(expectedUrl)) {
                 break;
             }
         }
@@ -32,19 +40,115 @@ public class BrowserUtils {
     /**
      * switches to the window by the exact title
      * return to original window if the window with given title not found
+     *
      * @param driver
      * @param targetTitle
      */
-    public static void  switchToWindow(WebDriver driver, String targetTitle){
+    public static void switchToWindow(WebDriver driver, String targetTitle) {
         String origin = driver.getWindowHandle();
-        for (String handle :driver.getWindowHandles()){
+        for (String handle : driver.getWindowHandles()) {
             driver.switchTo().window(handle);
-            if (driver.getTitle().contains(targetTitle)){
+            if (driver.getTitle().contains(targetTitle)) {
                 return;
             }
         }
         driver.switchTo().window(origin);
 
+    }
+
+    /**
+     * @param driver
+     * @param expectedTitle returns void, assertion is implemented
+     * @author alex
+     */
+    public static void validateTitle(WebDriver driver, String expectedTitle) {
+        assertTrue(driver.getTitle().contains(expectedTitle));
+    }
+
+    /**
+     * Click any link from loop practice
+     *
+     * @param nameOfPage
+     * @author alex
+     */
+    public static void loopLinkClick(String nameOfPage) {
+        WebElement element = Driver.getDriver().findElement(By.xpath("//a[.='" + nameOfPage + "']"));
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
+
+    /**
+     * Moves the mouse to given element
+     *
+     * @param element on which to hover
+     * @author alex
+     */
+    public static void hover(WebElement element) {
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(element).perform();
+    }
+
+    /**
+     * Clicks on an element using JavaScript
+     *
+     * @param element
+     * @author alex
+     */
+    public static void clickWithJS(WebElement element) {
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].click();", element);
+    }
+
+    /**
+     * Scrolls down to an element using JavaScript
+     *
+     * @param element
+     * @author alex
+     */
+    public static void scrollToElement(WebElement element) {
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+    /**
+     * Performs double click action on an element
+     * @param element
+     * @author alex
+     */
+    public static void doubleClick(WebElement element){
+        new Actions(Driver.getDriver()).doubleClick(element).build().perform();
+    }
+    /**
+     * Waits for the provided element to be visible on the page
+     * @param element
+     * @param timeToWaitInSec
+     * @return
+     * @author alex
+     */
+    public static WebElement waitForVisibility(WebElement element, int timeToWaitInSec){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(timeToWaitInSec));
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+    /**
+     * performs a pause
+     * @param seconds
+     * @author nadir
+     */
+    public static void justWait (int seconds){
+        try{
+            Thread.sleep(seconds);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Waits for provided element to be clickable
+     * @param element
+     * @param timeout
+     * @return
+     * @author nadir
+     */
+    public static WebElement waitForClickable (WebElement element, int timeout){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(timeout));
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
 
